6e37fb227cb730b8e485884cf926f4fd2d1d82b6
Grimm <joshuajking84@gmail.com>
2021-12-20 13:23:10 -0800

    Add project files.

---
+++ CSVReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    static class CSVReader
+    {
+        // Ctor that loads taxtable.csv automatically
+        private static void CSVReader()
+        {
+            var taxtable = File.ReadAllLines(@"C:\Users\joshu\source\repos\FinalProject\Resources\taxtable.csv");
+            foreach (var line in taxtable)
+            {
+                var values = line.Split(',');
+
+            }
+
+        }
+    }
+}
